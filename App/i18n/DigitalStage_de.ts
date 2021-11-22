987657d515b6229657dc6eb556d414212fdd20f3
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-11-22 23:22:37 +0100

    Updated deps

---
+++ App/i18n/DigitalStage_de.ts
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>LoginDialog</name>
+    <message>
+        <location filename="../src/LoginDialog.ui" line="14"/>
+        <source>Digital Stage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LoginDialog.ui" line="51"/>
+        <source>Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LoginDialog.ui" line="58"/>
+        <source>Email:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LoginDialog.ui" line="65"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;No Account yet? &lt;a href=&quot;https://dev.dstage.org/account/signup&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0068da;&quot;&gt;Sign up here&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LoginDialog.ui" line="75"/>
+        <source>Sign in</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LoginDialog.ui" line="95"/>
+        <source>Please enter your email and password from test.digital-stage.org:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="33"/>
+        <source>Systray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="34"/>
+        <source>I couldn&apos;t detect any system tray on this system.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../src/TrayIcon.cpp" line="13"/>
+        <source>Digital Stage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/TrayIcon.cpp" line="19"/>
+        <source>Restart</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/TrayIcon.cpp" line="25"/>
+        <source>Open stage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/TrayIcon.cpp" line="30"/>
+        <source>Open settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/TrayIcon.cpp" line="36"/>
+        <source>Add Box</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/TrayIcon.cpp" line="42"/>
+        <source>Logout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/TrayIcon.cpp" line="48"/>
+        <location filename="../src/TrayIcon.cpp" line="61"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/TrayIcon.cpp" line="56"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
