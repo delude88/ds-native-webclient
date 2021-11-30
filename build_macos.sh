a8aedc329d0bad79d22fe52a842acf1bc81a4ff2
delude88 <tobias.hegemann@googlemail.com>
2021-11-30 11:54:09 +0100

    Using github tags as version

--- build_macos.sh
+++ build_macos.sh
@@ -1,14 +1,17 @@
 #!/bin/zsh
-# Prepare
-#git submodule update --init --recursive
-#pip3 install wheel setuptools
-#pip3 install aqtinstall conan
-# Install Qt
-#aqt install-qt mac desktop 6.2.1 -O ./build/Qt
-# Instal other dependencies using conan
-#conan install -if build --build missing .
-# Configure
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SERVICE=OFF -DCMAKE_MODULE_PATH=./build/Qt6/6.2.1/macos/lib/cmake -DQt6_DIR=./build/Qt6/6.2.1/macos/lib/cmake/Qt6 -DCODESIGN_CERTIFICATE_NAME="Developer ID Application: Tobias Hegemann (JH3275598G)"
+## You can skip preparation by calling ./build_macos.sh noprepare
+if [ "$1" != "noprepare" ]; then
+  # Prepare
+  git submodule update --init --recursive
+  pip3 install wheel setuptools
+  pip3 install aqtinstall conan
+  # Install Qt
+  aqt install-qt mac desktop 6.2.1 -O ./build/Qt
+  # Instal other dependencies using conan
+  conan install -if build --build missing .
+  # Configure
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SERVICE=OFF -DCMAKE_MODULE_PATH=./build/Qt6/6.2.1/macos/lib/cmake -DQt6_DIR=./build/Qt6/6.2.1/macos/lib/cmake/Qt6 -DCODESIGN_CERTIFICATE_NAME="Developer ID Application: Tobias Hegemann (JH3275598G)"
+fi
 # Build
 cmake --build build --config Release --parallel
 # Pack
