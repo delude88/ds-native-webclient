4622b5f1a75bfca6470553e9a250b8f3a29bb03d
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-12-03 09:36:17 +0100

    Using brew instead of aqinstall for macos

--- build_macos.sh
+++ build_macos.sh
@@ -4,20 +4,21 @@ if [ "$1" != "noprepare" ]; then
   # Prepare
   git submodule update --init --recursive
   pip3 install wheel setuptools
-  pip3 install aqtinstall conan
+  pip3 install conan
   # Install Qt
-  aqt install-qt mac desktop 6.2.1 -O ./build/Qt
+  #aqt install-qt mac desktop 6.2.1 -O ./build/Qt
+  brew install qt
   # Instal other dependencies using conan
   conan install -if build --build missing .
   # Configure
-  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH=./build/Qt6/6.2.1/macos/lib/cmake -DQt6_DIR=./build/Qt6/6.2.1/macos/lib/cmake/Qt6 -DCODESIGN_CERTIFICATE_NAME="Developer ID Application: Tobias Hegemann (JH3275598G)"
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCODESIGN_CERTIFICATE_NAME="Developer ID Application: Tobias Hegemann (JH3275598G)"
 fi
 # Build
-#cmake --build build --config Release --parallel --target DigitalStageConnectorApp
-cmake --build build --config Release --target DigitalStageConnectorApp
+cmake --build build --config Release --parallel --target DigitalStageConnectorApp
+#/opt/homebrew/opt/qt/bin/macdeployqt "build/bin/Digital Stage Connector.app" -verbose=1 -always-overwrite -sign-for-notarization="Developer ID Application: Tobias Hegemann (JH3275598G)"
 # Pack app
-cpack --config build/CPackConfigApp.cmake -B build
+#cpack --config build/CPackConfigApp.cmake -B build
 # Pack service
 # cpack --config build/CPackConfigService.cmake -B build
-codesign --force --options runtime --sign "Developer ID Application: Tobias Hegemann (JH3275598G)" build/Install-Connector.dmg
-spctl -a -t open -vvv --context context:primary-signature build/Install-Connector.dmg
+#codesign --force --options runtime --sign "Developer ID Application: Tobias Hegemann (JH3275598G)" build/Install-Connector.dmg
+#spctl -a -t open -vvv --context context:primary-signature build/Install-Connector.dmg
