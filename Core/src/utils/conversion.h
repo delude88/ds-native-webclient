cf9ac5f6af3694a9ec97d3e6cc5cf2187e9d75c5
delude88 <tobias.hegemann@googlemail.com>
2022-05-25 10:04:33 +0200

    Modified urls

--- Core/src/utils/conversion.h
+++ Core/src/utils/conversion.h
@@ -7,11 +7,56 @@
 
 #include <vector>
 
+static void floatToByte(unsigned char *arr, float value) {
+  int i = *reinterpret_cast<int *>(&value);
+  arr[0] = i & 0x00FF;
+  arr[1] = (i >> 8) & 0x00FF;
+  arr[2] = (i >> 16) & 0x00FF;
+  arr[3] = (i >> 24);
+}
+
+static float byteToFloat(unsigned char *arr) {
+  int i = arr[0] | (arr[1] << 8) | (arr[2] << 16) | (arr[3] << 24);
+  return *(float *) &i;
+}
+
+static size_t serialize3(const float *input, const size_t size, std::byte *out) {
+  size_t out_size = size * 4;
+  for (size_t i = 0; i < size; i++) {
+    auto *buffer = reinterpret_cast<unsigned char *>(&out[i * 4]);
+    floatToByte(buffer, input[i]);
+  }
+  return out_size;
+}
+
+static size_t serialize2(const float *input, const size_t size, std::byte *out) {
+  size_t out_size = size * 4;
+  uint32_t temp = 0;
+  for (size_t i = 0; i < size; i++) {
+    const auto *buffer = reinterpret_cast<const unsigned char *>(&input[i]);
+#if defined (DS_LITTLE_ENDIAN)
+    temp = ((buffer[3] >> 24 & 0x00FF) |
+        (buffer[2] >> 16 & 0x00FF) |
+        (buffer[1] >> 8 & 0x00FF) |
+        buffer[0] & 0x00FF);
+#else
+    temp = ((buffer[ß] >> 24 & 0x00FF) |
+        (buffer[1] >> 16 & 0x00FF) |
+        (buffer[2] >> 8 & 0x00FF) |
+        buffer[3] & 0x00FF);
+#endif
+    out[i * 4] = *(reinterpret_cast<std::byte *>(&temp));
+  }
+  return out_size;
+}
+
 static size_t serialize(const float *input, const size_t size, std::byte *out) {
   size_t out_size = size * 4;
   for (size_t i = 0; i < size; i++) {
+    //auto value = input[i];
+    //auto *pointer = reinterpret_cast<int *>(&value); // get pointer to first byte of current index
     auto *pointer = (unsigned char *) &input[i]; // get pointer to first byte of current index
-    auto *buffer = (unsigned char *) &out[i * 4];  // access out using uchar ptr
+    auto *buffer = reinterpret_cast<unsigned char *>(&out[i * 4]);  // access out using uchar ptr
 #if defined (DS_LITTLE_ENDIAN)
     buffer[0] = pointer[0];
     buffer[1] = pointer[1];
